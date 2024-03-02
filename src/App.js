a9f629768a31f76151be5617326e89b792c19c8e
dorian-palace <dorian.palace@laplateforme.io>
2024-03-02 18:49:23 +0100

    react paginate

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
 
   let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
   let [fetchData, updateFetchedData] = useState([]);
   let {info, results} = fetchData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   useEffect(() => {
 
@@ -27,6 +29,8 @@ function App() {
 
       <h1 className="text-center ubuntu my-4" >Rick & Morty <span className="text-primary">Wiki</span> </h1>
 
+      <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+
       <div className="container">
         <div className="row">
           <div className="col-3">
@@ -40,7 +44,7 @@ function App() {
         </div>
       </div>
 
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
 }
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import ReactPaginate from "react-paginate";
 
-const Pagination = ({ pageNumber, setPageNumber }) => {
+const Pagination = ({info, pageNumber, setPageNumber }) => {
 
-    let next = () => {
-        setPageNumber(x => x + 1);
-    };
-    let prev = () => {
-        if (pageNumber === 1) return;
-        setPageNumber(x => x - 1);
-    };
-
-  return (
-    <div className="container d-flex justify-content-center gap-5 my-5">
-
-        <button onClick={prev} className="btn btn-primary">Prev</button>
-        <button onClick={next} className="btn btn-primary">Next</button>
-
-    </div>
-  )
-}
+  return <ReactPaginate 
+  forcePage={pageNumber === 1? 0 : pageNumber -1}
+  className="pagination justify-content-center gap-4 my-4"
+  nextLabel={<span style={{ color: '#ffffff' }}>Next</span>}
+  previousLabel={<span style={{ color: '#ffffff' }}>Prev</span>}
+  previousClassName="btn btn-primary fs-5 prev"
+  nextClassName="btn btn-primary fs-5 next"
+  pageClassName="page-item"
+  pageLinkClassName="page-link"
+  activeClassName="active"
+  onPageChange={(data)=>{setPageNumber(data.selected + 1)}}
+  pageCount={info?.pages} />;
+};
 
 export default Pagination
